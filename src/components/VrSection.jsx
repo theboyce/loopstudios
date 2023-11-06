c59751f87b79f470cc2201811307263cb6079652
theboyce <kwabenaadofo330@gmail.com>
2023-11-05 15:43:39 -1200

    changed vr image path

--- src/components/VrSection.jsx
+++ src/components/VrSection.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import VrImage from '../assets/images/mobile/image-interactive.jpg';
 
 const VrSection = () => {
   return (
     <div className=" md:px-[7rem] h-[100vh] flex justify-center items-center">
       <div className="sm:flex-col lg:flex-row flex items-end">
         <div className=" flex-1">
-          <img
-            src="/src/assets/images/image-interactive.jpg"
-            alt=""
-            className="px-[1.5rem] lg:p-0 w-full"
-          />
+          <img src={VrImage} alt="" className="px-[1.5rem] lg:p-0 w-full" />
         </div>
         <div className="translate-x-0 flex-1 bg-white sm:h-auto lg:h-[350px] flex justify-center items-end lg:-translate-x-[12rem]">
           <div className="text-center px-[1.5rem] flex flex-col gap-6 py-8 lg:w-[80%">
